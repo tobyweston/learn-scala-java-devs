275cd3c80b56eb92291dd69d6bbdb481331d414f
Toby Weston <toby.weston@gmail.com>
2017-09-15 18:07:57 +0100

    varargs

---
+++ src/main/java/s4j/java/chapter05/VarArgs.java
@@ -0,0 +1,11 @@
+package s4j.java.chapter05;
+
+public class VarArgs {
+
+	public void add(String... names) { }
+
+	// scala
+	//
+	// def add(names: String*) = ???
+
+}
---
+++ src/main/scala/s4j/scala/chapter05/VarArgs.scala
@@ -0,0 +1,10 @@
+package s4j.scala.chapter05
+
+object VarArgs {
+
+  // java
+  //
+  // public void add(String... names) { }
+
+  def add(names: String*) = ???
+}
