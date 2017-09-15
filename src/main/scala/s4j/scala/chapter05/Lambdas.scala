4e6d4382dfff4bbe9c63f6733d10aeef5b8686aa
Toby Weston <toby.weston@gmail.com>
2017-09-15 18:37:26 +0100

    lambdas example

---
+++ src/main/scala/s4j/scala/chapter05/Lambdas.scala
@@ -0,0 +1,28 @@
+package s4j.scala.chapter05
+
+object Lambdas {
+
+  val tuesday = true
+
+  def test(f: () => Boolean) = f.apply
+
+  // pass in a function literal
+  test(() => if (!tuesday) true else false)
+
+
+  // alternative example
+  //
+  // this will take a predicate (String => Boolean) and test
+  // it against a value (in our case "monday").
+  //
+  def test(f: String => Boolean): Boolean = {
+    // get some value
+    val dayOfWeek = "monday"
+    f(dayOfWeek)
+  }
+
+  // call it with a function literal
+  test(value => if (value == "tuesday") true else false)
+
+
+}
