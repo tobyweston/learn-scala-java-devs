349f641c909efbe052bc986046134c988027307d
Toby Weston <toby.weston@gmail.com>
2017-09-15 18:11:34 +0100

    default args

---
+++ src/main/scala/s4j/scala/chapter05/DefaultArgs.scala
@@ -0,0 +1,12 @@
+package s4j.scala.chapter05
+
+object DefaultArgs {
+
+  swap(3)                       // (1, 3)
+  swap(3, 2)                    // (2, 3)
+  swap(first = 3)               // (1, 3)
+  swap(first = 3, second = 1)   // (1, 3)
+
+  def swap(first: Int, second: Int = 1) = (second, first)
+
+}
