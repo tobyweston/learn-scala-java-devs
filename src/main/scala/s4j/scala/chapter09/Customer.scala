d9161f11a41bef9c9792e8f9140751b3dae9937a
Toby Weston <toby.weston@gmail.com>
2017-09-15 18:51:33 +0100

    add decompiler output (and decompiler jar)

--- src/main/scala/s4j/scala/chapter09/Customer.scala
+++ src/main/scala/s4j/scala/chapter09/Customer.scala
@@ -5,3 +5,33 @@ class Customer(val name: String, val address: String)
 object Customer {
   val eric = new Customer("Eric", "29 Acacia Road")
 }
+
+/* decompile output
+
+java -jar cfr_0_122.jar target/scala-2.12/classes/s4j/scala/chapter09/Customer.class
+
+package s4j.scala.chapter09;
+
+public class Customer {
+  private final String name;
+  private final String address;
+
+  public static Customer eric() {
+    return Customer$.MODULE$.eric();
+  }
+
+  public String name() {
+    return this.name;
+  }
+
+  public String address() {
+    return this.address;
+  }
+
+  public Customer(String name, String address) {
+    this.name = name;
+    this.address = address;
+  }
+}
+
+*/
