04b60219153f1185dbe5a08834c8d08e7de01787
Toby Weston <toby.weston@gmail.com>
2017-09-16 14:35:03 +0100

    add trivial ternary/expression example

---
+++ src/main/java/s4j/java/chapter13/Ternary.java
@@ -0,0 +1,11 @@
+package s4j.java.chapter13;
+
+@SuppressWarnings("all")
+public class Ternary {
+
+    static {
+        Integer height = 90;
+
+        String tall = height > 190 ? "tall" : "not tall";
+    }
+}
---
+++ src/main/scala/s4j/scala/chapter13/Ternary.scala
@@ -0,0 +1,7 @@
+package s4j.scala.chapter13
+
+object Ternary {
+  val height = 90
+
+  val tall = if (height > 190) "tall" else "not tall"
+}
