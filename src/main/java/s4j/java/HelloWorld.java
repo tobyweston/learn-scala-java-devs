98b5d266f7996c7d818643695d10cb5fcb74bbc9
Toby Weston <toby.weston@gmail.com>
2017-09-16 16:21:54 +0100

    hello world!

---
+++ src/main/java/s4j/java/HelloWorld.java
@@ -0,0 +1,7 @@
+package s4j.java;
+
+public class HelloWorld {
+	public static void main(String... args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ src/main/scala/s4j/scala/chapter02/HelloWorld.scala
@@ -0,0 +1,7 @@
+package s4j.scala.chapter02
+
+object HelloWorld extends App {
+
+  println("Hello World!")
+
+}
