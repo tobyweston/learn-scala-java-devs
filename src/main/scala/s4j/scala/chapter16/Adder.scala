9b2ea963e3bc78c2839113efa98c7f407a4607f7
Toby Weston <toby.weston@gmail.com>
2017-09-22 21:45:29 +0100

    Adder class

---
+++ src/main/scala/s4j/scala/chapter16/Adder.scala
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2015-2017 Toby Weston
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package s4j.scala.chapter16
+
+class Adder {
+  def apply(a: Int, b: Int) = a + b
+}
+
+object Adder {
+  val add = new Adder()
+  add.apply(1, 3)
+  add(1, 3)
+}
