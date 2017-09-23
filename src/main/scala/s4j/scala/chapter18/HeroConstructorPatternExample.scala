5c6a58be453d5d23743e5cf2d56b87af3a004765
Toby Weston <toby.weston@gmail.com>
2017-09-23 15:19:52 +0100

    tweak

--- src/main/scala/s4j/scala/chapter18/HeroConstructorPatternExample.scala
+++ src/main/scala/s4j/scala/chapter18/HeroConstructorPatternExample.scala
@@ -18,22 +18,22 @@ package s4j.scala.chapter18
 
 object HeroConstructorPatternExample extends App {
 
-  val bruce = new SuperHero("Batman", "Bruce Wayne", List("Intellect", "Speed", "Agility", "Strength"))
-  val youngBruce = new Person("Bruce Wayne")
+  val bruce = new SuperHero("Batman", "Bruce Wayne", List("Speed", "Agility"))
+  val steve = new SuperHero("Captain America", "Steve Rogers", List("Tactics", "Speed"))
   val jane = new Person("Jane Doe")
+
 
   def superPowersFor(person: Person) = {
     person match {
-      case SuperHero(_, name, powers) => powers
-      case _                          => List()
+      case SuperHero(_, _, powers) => powers
+      case _                       => List()
     }
   }
 
-  // Example of a constructor pattern
+  // What super-powers does an otherwise unknown person have, if they are a
+  // superhero with the alter-ego Bruce Wayne?
 
-  // What super powers does a person have, if they are in fact a super hero who's alter ego is Bruce Wayne?
-  val person = bruce
-  println("Bruce has the following powers " + superPowersFor(person))
-  println("Young Bruce has the following powers " + superPowersFor(youngBruce))
-  println("Jane has the following powers " + superPowersFor(jane))
+  println("Bruce has the following powers: " + superPowersFor(bruce).mkString(", "))
+  println("Steve has the following powers: " + superPowersFor(steve).mkString(", "))
+  println("Jane has the following powers: " + superPowersFor(jane).mkString(", "))
 }
